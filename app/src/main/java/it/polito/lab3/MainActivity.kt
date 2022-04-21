6b42b6378b4ae307c2f441a03cdf8c18412644ad
biola99 <64309231+biola99@users.noreply.github.com>
2022-04-21 09:26:23 +0200

    add EditProfileFragment

--- app/src/main/java/it/polito/lab3/MainActivity.kt
+++ app/src/main/java/it/polito/lab3/MainActivity.kt
@@ -2,10 +2,34 @@ package it.polito.lab3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
- // Main activity, is the base for all the fragments
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+// Main activity, is the base for all the fragments
 class MainActivity : AppCompatActivity() {
+    var drawerLayout: DrawerLayout? = null
+    var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout?.addDrawerListener(actionBarDrawerToggle!!)
+        actionBarDrawerToggle!!.syncState()
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+
+        //if drawerIcon clicked
+        if(actionBarDrawerToggle!!.onOptionsItemSelected(item)){
+            return true
+        }
+        else return super.onOptionsItemSelected(item)
     }
 }
