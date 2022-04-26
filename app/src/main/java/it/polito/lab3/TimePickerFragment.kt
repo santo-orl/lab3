0ac3c88aca6d4e5ef508352ee4d480e3d1722233
Simona Chiurato <simona.chiurato@yahoo.it>
2022-04-26 15:08:23 +0200

    modifiche time

--- app/src/main/java/it/polito/lab3/TimePickerFragment.kt
+++ app/src/main/java/it/polito/lab3/TimePickerFragment.kt
@@ -3,39 +3,86 @@ package it.polito.lab3
 
 import android.app.Dialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.DEBUG
 import android.widget.TimePicker
 
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.properties.Delegates
 
 class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
     private val calendar = Calendar.getInstance()
+    private var hour by Delegates.notNull<Int>()
+    private var minute by Delegates.notNull<Int>()
+    private var from:String = ""
+    private var to:String = ""
 
+    private lateinit var sharedPref:SharedPreferences
+    private val sharedPrefFIle = "it.polito.fragment"
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        super.onCreateDialog(savedInstanceState)
+        sharedPref = requireActivity().getSharedPreferences(sharedPrefFIle, Context.MODE_PRIVATE)
         // default date
-
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val minute = calendar.get(Calendar.MINUTE)
-
-
-        // return new DatePickerDialog instance
-        return TimePickerDialog(requireActivity(), this, hour,minute,false)
+        if (sharedPref.getString("FROM_TIME","").toString() != "") {
+            Log.d("testing from2",from)
+            from = sharedPref.getString("FROM_TIME","").toString()
+            if (sharedPref.getString("TO_TIME","").toString() != "") {
+                to = sharedPref.getString("TO_TIME","").toString()
+            }
+            hour =from.split(":")[0].toInt()
+            minute = from.split(":")[1].toInt()
+            Log.d("testing create",from)
+        }else{
+            hour = calendar.get(Calendar.HOUR_OF_DAY)
+            minute = calendar.get(Calendar.MINUTE)
+        }
+        return TimePickerDialog(requireActivity(), this, hour ,minute,true)
     }
 
+ /* override fun onSaveInstanceState(outState: Bundle) {
+        Log.d("testing_saved", "")
+        outState.putString("FROM_TIME",from)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        if (savedInstanceState != null) {
+            Log.d("testing", savedInstanceState.getString("FROM_TIME", ""))
+            from = savedInstanceState.getString("FROM_TIME", "")
+        }
+    }*/
+
     override fun onTimeSet(view: TimePicker?, hour: Int, minute: Int) {
+        Log.d("testing on time","")
         calendar.set(Calendar.HOUR_OF_DAY, hour)
         calendar.set(Calendar.MINUTE, minute)
-
-
-        val selectedTime = SimpleDateFormat(" HH:mm", Locale.ENGLISH).format(calendar.time)
-
         val selectedTimeBundle = Bundle()
-        selectedTimeBundle.putString("SELECTED_TIME", selectedTime)
+        val selectedTime = SimpleDateFormat("HH:mm", Locale.ENGLISH).format(calendar.time)
+        if(from != ""){
+            Log.d("testing on time1",from)
+            selectedTimeBundle.putString("FROM_TIME", from)
+            selectedTimeBundle.putString("TO_TIME", selectedTime)
+            to = selectedTime
+        }else{
+            Log.d("testing on time2",from)
+            selectedTimeBundle.putString("FROM_TIME", selectedTime)
+            from = selectedTime
+        }
+        Log.d("testing on time3",from)
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putString("FROM_TIME", from)
+        editor.putString("TO_TIME", to)
+        editor.apply()
+        setFragmentResult("KeyTime", selectedTimeBundle)
 
-        setFragmentResult("REQUEST_KEY", selectedTimeBundle)
     }
 }
