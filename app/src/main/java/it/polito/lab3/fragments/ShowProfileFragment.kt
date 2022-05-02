71ab6c37e26ea9017482de85f5006723738c86a0
santo-orl <orlandosantino11@gmail.com>
2022-05-02 10:48:19 +0200

    aggiunta gestione fotocamera :)

--- app/src/main/java/it/polito/lab3/fragments/ShowProfileFragment.kt
+++ app/src/main/java/it/polito/lab3/fragments/ShowProfileFragment.kt
@@ -1,16 +1,40 @@
 package it.polito.lab3.fragments
 
+import android.content.Context
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import it.polito.lab3.R
+import it.polito.lab3.skills.Adapter_Text
+import it.polito.lab3.skills.Skill
 
 
 class ShowProfileFragment : Fragment(R.layout.fragment_show_profile) {
+
+    var name: String = "Full name"
+    var nickname: String = "Nickname"
+    var location: String = "Location"
+    private var email: String = "email@address"
+    private var skillList: ArrayList<Skill> = arrayListOf()
+    private lateinit var uriImage: Uri
+    private var uriImageString: String = ""
+    //associated fields to layout
+    lateinit var name_field: TextView
+    lateinit var nickname_field: TextView
+    lateinit var location_field: TextView
+    lateinit var email_field: TextView
+    lateinit var photo_field: ImageView
+    private lateinit var state: Parcelable
+    //per differenziare i due recycler quando mostra le skills
+    private lateinit var adapterText: Adapter_Text
 
     private val sharedPrefFIle = "it.polito.showprofileactivityy"
     lateinit var sharedPref: SharedPreferences;
@@ -32,6 +56,8 @@ class ShowProfileFragment : Fragment(R.layout.fragment_show_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPref = this.requireActivity().getSharedPreferences(sharedPrefFIle, Context.MODE_PRIVATE)
+
     }
 
     //creo la pencil icon in alto a dx
