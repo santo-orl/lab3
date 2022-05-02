8fcd966bfc0ec6befec42b07170bf04b2acd17d4
Leandra <valentileandra@gmail.com>
2022-05-02 16:34:59 +0200

    save fields from edit profile

--- app/src/main/java/it/polito/lab3/fragments/ProfileViewModel.kt
+++ app/src/main/java/it/polito/lab3/fragments/ProfileViewModel.kt
@@ -1,15 +1,44 @@
 package it.polito.lab3.fragments
 
+import android.net.Uri
 import android.text.Editable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProfileViewModel: ViewModel() {
-    val name = MutableLiveData<String>().also{it.value = "PROVA"}
-    //val name: LiveData<String> = _name
+    private val _name = MutableLiveData<String>().also{it.value = "Full name"}
+    val name: LiveData<String> = _name
+
+    private val _nickname = MutableLiveData<String>().also{it.value = "Nickname"}
+    val nickname: LiveData<String> = _nickname
+
+    private val _email = MutableLiveData<String>().also{it.value = "email@address"}
+    val email: LiveData<String> = _email
+
+    private val _location = MutableLiveData<String>().also{it.value = "Location"}
+    val location: LiveData<String> = _location
+
+    private val _photoString = MutableLiveData<String>().also{it.value = ""}
+    var photoString: LiveData<String> = _photoString
 
     fun setName(desiredName: String){
-        name.value = desiredName
+        _name.value = desiredName
+    }
+
+    fun setNickname(desiredNickname: String){
+        _nickname.value = desiredNickname
+    }
+
+    fun setEmail(desiredEmail: String){
+        _email.value = desiredEmail
+    }
+
+    fun setLocation(desiredLocation: String){
+        _location.value = desiredLocation
+    }
+
+    fun setPhoto(desiredUri: String){
+        _photoString.value = desiredUri
     }
 }
