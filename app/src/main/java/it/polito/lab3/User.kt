4735f13ee344a00acc5726c6e5c5100a5c2fb9da
Simona Chiurato <simona.chiurato@yahoo.it>
2022-05-08 09:53:23 +0200

    codice connessione con google ma non funge

---
+++ app/src/main/java/it/polito/lab3/User.kt
@@ -0,0 +1,16 @@
+package it.polito.lab3
+import android.os.Parcelable
+import it.polito.lab3.skills.Skill
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(var name: String, var nickname: String, var email: String,
+                var location: String,
+                var  photoString: String, var skills: ArrayList<Skill>
+) : Parcelable {
+
+    override fun toString(): String {
+        return ""
+    }
+
+}
--- app/src/main/java/it/polito/lab3/fragments/ProfileViewModel.kt
+++ app/src/main/java/it/polito/lab3/fragments/ProfileViewModel.kt
@@ -1,13 +1,21 @@
 package it.polito.lab3.fragments
 
 import android.net.Uri
+import android.provider.ContactsContract
 import android.text.Editable
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import it.polito.lab3.User
 import it.polito.lab3.skills.Skill
 
 class ProfileViewModel: ViewModel() {
+    private val db: FirebaseFirestore
+    init {
+        db= FirebaseFirestore.getInstance()
+    }
     private val _name = MutableLiveData<String>("")
     val name: LiveData<String> = _name
 
@@ -26,6 +34,17 @@ class ProfileViewModel: ViewModel() {
     private val _skills = MutableLiveData<ArrayList<Skill>>(arrayListOf())
     var skills: LiveData<ArrayList<Skill>> =_skills
 
+    fun createUser(name:String, nickname: String, email: String,
+                    location: String,
+                     photoString: String,  skills: ArrayList<Skill> ){
+        val user = User(name, nickname, email, location, photoString,  skills)
+        db.collection("users").add(user).addOnSuccessListener { documentReference ->
+            Log.i("test","DocumentSnapshot added with ID:${documentReference.id}")
+        }
+            .addOnFailureListener{e ->
+                Log.i("test","Error adding document",e)
+            }
+    }
     fun setName(desiredName: String){
         _name.value = desiredName
     }
