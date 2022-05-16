7f81b6399f4cfd2d8679c7cd9477e11e02d38750
Leandra <valentileandra@gmail.com>
2022-05-16 11:06:16 +0200

    search skills (3)

--- app/src/main/java/it/polito/lab4/fragments/HomeFragment.kt
+++ app/src/main/java/it/polito/lab4/fragments/HomeFragment.kt
@@ -13,6 +13,7 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import it.polito.lab4.ProfileViewModel
 import it.polito.lab4.R
 import it.polito.lab4.skills.Skill
@@ -54,12 +55,17 @@ class HomeFragment : Fragment(R.layout.fragment_home_skilllist) {
 
         search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(p0: String?): Boolean {
-                searchSkills(search_view.query.toString(),id)
+                Log.i("QUERY1", search_view.query.toString())
+                searchSkills(search_view.query.toString(), id)
                 Toast.makeText(context, search_view.query, Toast.LENGTH_SHORT).show()
                 return true
             }
 
             override fun onQueryTextChange(p0: String?): Boolean {
+
+                    this.onQueryTextSubmit("")
+
+                //searchSkills(search_view.query.toString(), id)
                 //Toast.makeText(context,search_view.query,Toast.LENGTH_SHORT).show()
                 return true
             }
@@ -164,38 +170,87 @@ class HomeFragment : Fragment(R.layout.fragment_home_skilllist) {
         super.onViewStateRestored(savedInstanceState)
     }
 
+    private fun visualizeSkills(result: QueryDocumentSnapshot){
+
+    }
+
     private fun searchSkills(query: String, id: String) {
         skillList = arrayListOf()
+        Log.i("QUERY", query)
+        if(query!="") {
+            db.collection("skills").whereEqualTo("$query" + ".title", query).get()
+                .addOnSuccessListener {
+                    //db.collection("skills").orderBy(query).get().addOnSuccessListener {
+                        result ->
+                    for (document in result) {
+                        Log.i("TEST_DOCUMENT", "$document")
 
-        db.collection("skills").whereEqualTo("title",query).get().addOnSuccessListener {
-            //Log.i("test_SKILLS", "STO CERCANDO NEL DATABASE ")
-            result->
-            for (document in result) {
-
-                document.data.forEach { (c, s) ->
-                    Log.i("test_home", s.toString())
-                    s as HashMap<*, *>
-                    skillList.add(
-                        Skill(
-                            s["title"].toString(),
-                            s["description"].toString(),
-                            s["pos"].toString().toInt()
+                        if (document.id != id) {
+                            document.data.forEach { (c, s) ->
+                                Log.i("test_home", s.toString())
+                                s as HashMap<*, *>
+                                if (s["title"] == query) {
+                                    skillList.add(
+                                        Skill(
+                                            s["title"].toString(),
+                                            s["description"].toString(),
+                                            s["pos"].toString().toInt()
+                                        )
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    if (skillList.isEmpty()) {
+                        Log.i("testList", skillList.toString())
+                        skillList.add(
+                            Skill(
+                                "No skills found",
+                                "No one has the skill you are searching for!",
+                                0
+                            )
                         )
-                    )
+                    }
                 }
-
-
+                .addOnFailureListener { exception ->
+                    Log.i("test_SKILLS", "Error getting SKILLS: ", exception)
+                }
         }
-            if (skillList.isEmpty()) {
-                Log.i("testList", skillList.toString())
-                skillList.add(
-                    Skill(
-                        "No skills available",
-                        "Other users have not yet placed advertisements, be the first, add it in your profile!",
-                        0
-                    )
-                )
-            }
+        if(query=="") {
+            //prende tutte le skills
+            db.collection("skills")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        if (document.id != id) {
+                            document.data.forEach { (c, s) ->
+                                Log.i("test_home", s.toString())
+                                s as HashMap<*, *>
+                                skillList.add(
+                                    Skill(
+                                        s["title"].toString(),
+                                        s["description"].toString(),
+                                        s["pos"].toString().toInt()
+                                    )
+                                )
+                            }
+
+                        }
+                    }
+                    if (skillList.isEmpty()) {
+                        Log.i("testList", skillList.toString())
+                        skillList.add(
+                            Skill(
+                                "No advertisements yet!",
+                                "No one has posted a skill, be the first to add one!",
+                                0
+                            )
+                        )
+                    }
+                }
+        }
+
+
             Log.i("testList2", skillList.toString())
             recycler_view.layoutManager = LinearLayoutManager(this.activity)
             adapterSkill = Adapter_homeFrg(skillList)
@@ -207,18 +262,15 @@ class HomeFragment : Fragment(R.layout.fragment_home_skilllist) {
                 }
 
                 override fun onSkillDeleted(position: Int) {
-
                 }
 
             })
 
         }
-        .addOnFailureListener { exception ->
-            Log.i("test_SKILLS", "Error getting SKILLS: ", exception)
-        }
-    }
+
 
 }
+
 
 
 
