79d09ced381ef4121be178a9fd4645ae086ab4f3
SimonaChiurato <s248267@studenti.polito.it>
2022-05-30 21:17:13 +0200

    inizio modifiche db

--- app/src/main/java/it/polito/lab4/fragments/HelpFragment.kt
+++ app/src/main/java/it/polito/lab4/fragments/HelpFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import it.polito.lab4.R
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
 
 class HelpFragment : Fragment(R.layout.fragment_help) {
@@ -27,6 +32,56 @@ class HelpFragment : Fragment(R.layout.fragment_help) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         activity?.title = "Help"
+
+        var profile_button = view.findViewById<Button>(R.id.buttonProfile)
+        var slot_button = view.findViewById<Button>(R.id.buttonSlot)
+        var home_button = view.findViewById<Button>(R.id.buttonHome)
+        var chat_button = view.findViewById<Button>(R.id.buttonChat)
+        var done_button = view.findViewById<Button>(R.id.buttonJobDone)
+
+
+
+        profile_button.setOnClickListener {
+            val activity = it.context as? AppCompatActivity
+            activity?.supportFragmentManager?.commit {
+                addToBackStack(ShowProfileFragment::class.toString())
+                setReorderingAllowed(true)
+                replace<ShowProfileFragment>(R.id.myNavHostFragment)
+            }
+        }
+        slot_button.setOnClickListener {
+            val activity = it.context as? AppCompatActivity
+            activity?.supportFragmentManager?.commit {
+                addToBackStack(ListSkillUserFragment::class.toString())
+                setReorderingAllowed(true)
+                replace<ListSkillUserFragment>(R.id.myNavHostFragment)
+            }
+        }
+        home_button.setOnClickListener {
+            val activity = it.context as? AppCompatActivity
+            activity?.supportFragmentManager?.commit {
+                addToBackStack(HomeFragment::class.toString())
+                setReorderingAllowed(true)
+                replace<HomeFragment>(R.id.myNavHostFragment)
+            }
+        }
+        chat_button.setOnClickListener {
+            val activity = it.context as? AppCompatActivity
+            activity?.supportFragmentManager?.commit {
+                addToBackStack(ChatListFragment::class.toString())
+                setReorderingAllowed(true)
+                replace<ChatListFragment>(R.id.myNavHostFragment)
+            }
+        }
+        done_button.setOnClickListener {
+            val activity = it.context as? AppCompatActivity
+            activity?.supportFragmentManager?.commit {
+                addToBackStack(AssignedAcceptedFragment::class.toString())
+                setReorderingAllowed(true)
+                replace<AssignedAcceptedFragment>(R.id.myNavHostFragment)
+            }
+        }
+
         super.onViewCreated(view, savedInstanceState)
     }
 
